764fbf247a20d29879ff5b740c92d4d7f5b05f4b
Ulises Montenegro <ulimontenegro23@gmail.com>
2023-11-25 20:58:29 -0300

    fighters added to the homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,9 @@ import kata from '@/assets/homepage/articles/karate_kata.jpg'
 /*Peleadores Images */
 import higa from '@/assets/homepage/peleadores/oscar_higa.jpg'
 import lidia from '@/assets/homepage/peleadores/lidia_rodriguez.jpg'
+import rafael from '@/assets/homepage/peleadores/aghayev_rafael.jpg'
+import yorgelis from '@/assets/homepage/peleadores/yorgelis_salazar.jpg'
+/* Componentes */
 import ArticleCard from '@/components/ArticleCard'
 import HeroHomepage from '@/components/homepage/Hero'
 import PeleadorCard from '@/components/homepage/PeleadorCard'
@@ -30,10 +33,10 @@ export default function Home() {
         <article className={styles.article}>
           <h2 className={styles.article__title}>Peleadores de Karate</h2>
           <div className={styles.article__peleador__container}>
-            <PeleadorCard peleadorImage={higa} nombre='Oscar Higa' texto='Oscar Higa es un peleador de karate nacido el 28 de marzo de 1945 en Buenos Aires' />
-            <PeleadorCard peleadorImage={lidia} nombre='Lidia Rodríguez' texto='Lidia Rodríguez Encabo en una peleadora de karate que compite en la modalidad de Kata, nacida el 23 de abril de 1997 en Alcalá, España.' />
-            <PeleadorCard peleadorImage={higa} nombre='Oscar Higa' texto='Oscar Higa es un peleador de karate nacido el 28 de marzo de 1945 en Buenos Aires' />
-            <PeleadorCard peleadorImage={lidia} nombre='Lidia Rodríguez' texto='Lidia Rodríguez Encabo en una peleadora de karate que compite en la modalidad de Kata, nacida el 23 de abril de 1997 en Alcalá, España.' />
+            <PeleadorCard peleadorImage={higa} nombre='Oscar Higa' texto='Oscar Higa es un peleador de karate nacido el 28 de marzo de 1945 en Buenos Aires...' />
+            <PeleadorCard peleadorImage={lidia} nombre='Lidia Rodríguez' texto='Lidia Rodríguez Encabo en una peleadora de karate que compite en la modalidad de Kata, nacida el 23 de abril de 1997 en Alcalá, España...' />
+            <PeleadorCard peleadorImage={rafael} nombre='Rafael Aghayev' texto='Rafael Aghayev es un karateka azerbayano nacido el 4 de marzo de 1985 especializado en kumite...' />
+            <PeleadorCard peleadorImage={yorgelis} nombre='Yorgelis Salazar' texto='Yorgelis Salazar es una karateka venezolana nacida el 10 de noviembre de 1997 que ganó una medalla de plata en los Juegos Mundiales de 2022...' />
           </div>
         </article>
       </section>
--- src/components/homepage/PeleadorCard.jsx
+++ src/components/homepage/PeleadorCard.jsx
@@ -5,8 +5,8 @@ export default function PeleadorCard ({peleadorImage, nombre, texto}) {
     return(
         <article className={styles.article__peleador}>
               <Image className={styles.article__peleador__image} src={peleadorImage} alt='' />
-              <h3 className={styles.article__peleador__title}>Oscar Higa</h3>
-              <p className={styles.article__peleador__text}>Oscar Higa es un peleador de karate nacido el 28 de marzo de 1945 en Buenos Aires</p>
+              <h3 className={styles.article__peleador__title}> {nombre} </h3>
+              <p className={styles.article__peleador__text}> {texto} </p>
         </article>
     )
 }
