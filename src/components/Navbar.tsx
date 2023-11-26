b899839fe9d4111e25eabbbdef03499e84351aa1
Ulises Montenegro <ulimontenegro23@gmail.com>
2023-11-26 18:31:56 -0300

    made the app completly responsive

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,16 +1,27 @@
+'use client'
+import { useState } from 'react'
 import Image from 'next/image'
 import styles from './navbar.module.css'
 import logo from '@/assets/KI.png'
 import Link from 'next/link'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faXmark, faBars } from '@fortawesome/free-solid-svg-icons'
 
 export default function Navbar () {
+    const [menu, setMenu] = useState(false)
     return(
         <nav className={styles.navbar}>
             <Link className={styles.navbar__logo} href='/'><Image src={logo} alt='' /></Link>
-            <ul className={`${styles.navbar__container}`}>
-                <Link className={styles.navbar__list} href='/'><li>Inicio</li></Link>
-                <Link className={styles.navbar__list} href='/peleadores'><li>Peleadores</li></Link>
-                <Link className={styles.navbar__list} href='/articulos'><li>Artículos</li></Link>
+            <div onClick={() => setMenu(!menu)} className={styles.navbar__menu} >
+                {menu ? 
+                <FontAwesomeIcon icon={faXmark} /> :
+                <FontAwesomeIcon icon={faBars} />
+                }
+            </div>
+            <ul className={menu ? `${styles.navbar__container} ${styles.active}` : `${styles.navbar__container}`}>
+                <Link onClick={() => setMenu(!menu)} className={styles.navbar__list} href='/'><li>Inicio</li></Link>
+                <Link onClick={() => setMenu(!menu)} className={styles.navbar__list} href='/peleadores'><li>Peleadores</li></Link>
+                <Link onClick={() => setMenu(!menu)} className={styles.navbar__list} href='/articulos'><li>Artículos</li></Link>
             </ul>
         </nav>
     )
