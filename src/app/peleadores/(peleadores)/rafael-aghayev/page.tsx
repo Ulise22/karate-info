418f917a21306beb3f5a0ac9f88c92a17075a94f
Ulises Montenegro <ulimontenegro23@gmail.com>
2023-11-29 20:54:43 -0300

    Rafael Aghayav article done

--- src/app/peleadores/(peleadores)/rafael-aghayev/page.tsx
+++ src/app/peleadores/(peleadores)/rafael-aghayev/page.tsx
@@ -1,9 +1,70 @@
-
+import ArticlesHero from '@/components/ArticlesHero'
+import styles from '../../components/peleador.module.css'
+import rafael from '@/assets/peleadores/rafael-aghayev.jpg'
 
 export default function Aghayev () {
     return(
-        <main>
-            <h1>Rafael Aghayev</h1>
+        <main className={styles.peleador}>
+            <ArticlesHero title='Rafael Aghayev' img={rafael} />
+            <section className={styles.peleador__article}>
+                <h2 className={styles.peleador__article__subtitle}>Aghayev Karate</h2>
+                <p className={styles.peleador__article__text}>Rafael Aghayev es un peleador de karate especializado en kumite, apodado por algunos como &quot;El messi del Karate&quot;, nació en Azerbayan el 4 de marzo de 1985. Se graduó como licenciado en la especialidad entrenador-instructor de karate en el marco del departamento de &quot;artes marciales y sus técnicas&quot;, en la facultad de artes marciales Academia de la Cultura Física y el Deporte en 2007. Durante un año, posterior a su graduación, se dedicó a servir a las fuerzas armadas en el Distrito de Ghaziabad, hasta que fue transferido al Club Central de Deportes del Ministerio de Defensa como deportista prometedor.</p>
+                <p className={styles.peleador__article__text}>Desde pequeño le fue inculcado su amor por el deporte por su padre, quien era un jugador de fútbol profesional. Por lo que en un inicio, Rafael comenzó a seguir los pasos de su padre al involucrarse en el mundo del fútbol desde los 7 años, a la par que entrenaba lo que acabaría siendo su verdadera pasión, el Karate, donde rapidamente destacaría al participar en competiciones nacionales.</p>
+                <h2 className={styles.peleador__article__subtitle}>Rafael Aghayev Medallas</h2>
+                <p className={styles.peleador__article__text}>A lo largo de su carrera ha ganado un sin fin de medallas, 26 de oro, 3 de plata y 12 de bronce. Aquí la lista:</p>
+                <h3>Oro</h3>
+                <ul className={styles.peleador__article__text}>
+                    <li><b>1.</b> Islamic Solidary Games (2013)</li>
+                    <li><b>2.</b>World Combate Games (2013)</li>
+                    <li><b>3.</b>Juegos Mundiales (2013)</li>
+                    <li><b>4.</b>Juegos Europeos (2015)</li>
+                    <li><b>5.</b>Karate1 Premier League(2012)</li>
+                    <li><b>6.</b>Karate1 Premier League(2013)</li>
+                    <li><b>7.</b>Karate1 Premier League(2016)</li>
+                    <li><b>8.</b>Karate1 Premier League(2017)</li>
+                    <li><b>9.</b>Campeonato Europeo de Karate (2002)</li>
+                    <li><b>10.</b>Campeonato Europeo de Karate (2003)</li>
+                    <li><b>11.</b>Campeonato Europeo de Karate (2004)</li>
+                    <li><b>12.</b>Campeonato Europeo de Karate (2005)</li>
+                    <li><b>13.</b>Campeonato Europeo de Karate (2005)</li>
+                    <li><b>14.</b>Campeonato Europeo de Karate (2007)</li>
+                    <li><b>15.</b>Campeonato Europeo de Karate (2007)</li>
+                    <li><b>16.</b>Campeonato Europeo de Karate (2008)</li>
+                    <li><b>17.</b>Campeonato Europeo de Karate (2009)</li>
+                    <li><b>18.</b>Campeonato Europeo de Karate (2010)</li>
+                    <li><b>19.</b>Campeonato Europeo de Karate (2013)</li>
+                    <li><b>20.</b>Campeonato Europeo de Karate (2015)</li>
+                    <li><b>21.</b>Campeonato Europeo de Karate (2016)</li>
+                    <li><b>22.</b>Campeonato Mundial de Karate (2006)</li>
+                    <li><b>23.</b>Campeonato Mundial de Karate (2008)</li>
+                    <li><b>24.</b>Campeonato Mundial de Karate (2008)</li>
+                    <li><b>25.</b>Campeonato Mundial de Karate (2010)</li>
+                    <li><b>26.</b>Campeonato Mundial de Karate (2016)</li>
+                </ul>
+                <h3>Plata</h3>
+                <ul className={styles.peleador__article__text}>
+                    <li><b>1.</b>Campeonato Europeo de Karate (2008)</li>
+                    <li><b>2.</b>Campeonato Mundial de Karate (2010)</li>
+                    <li><b>3.</b>Campeonato Mundial de Karate (2012)</li>
+                </ul>
+                <h3>Bronce</h3>
+                <ul className={styles.peleador__article__text}>
+                    <li><b>1.</b>Islamic Solidary Games (2005)</li>
+                    <li><b>2.</b>Karate1 Premier League (2015)</li>
+                    <li><b>3.</b>Karate1 Premier League (2016)</li>
+                    <li><b>4.</b>Karate1 Premier League (2016)</li>
+                    <li><b>5.</b>Campeonato Europeo de Karate (2008)</li>
+                    <li><b>6.</b>Campeonato Europeo de Karate (2009)</li>
+                    <li><b>7.</b>Campeonato Europeo de Karate (2010)</li>
+                    <li><b>8.</b>Campeonato Europeo de Karate (2011)</li>
+                    <li><b>9.</b>Campeonato Europeo de Karate (2012)</li>
+                    <li><b>10.</b>Campeonato Europeo de Karate (2012)</li>
+                    <li><b>11.</b>Campeonato Europeo de Karate (2015)</li>
+                    <li><b>12.</b>Campeonato Mundial de Karate (2003)</li>
+                </ul>
+                <p className={styles.peleador__article__text}>Rafael Aghayev ya destacó en su primer campeonato de la República con varias victorias aplastantes que hicieron llamar la atención del Presidente de la Federación Nacional de Karate, quien vio en él grandes cualidades. Finalmente Aghayev comenzó a formar parte de la Selección Nacional de su país a partir de 1997, donde ganó su primer campeonato en 1998 al participar del Campeonato Abierto de Inglaterra.</p>
+                <p className={styles.peleador__article__text}>Rafael hizó su debut profesional en 2003, y desde entonces ha ganado 41 medallas, siendo 26 de oro, 3 de plata, y 12 de bronce. Además de disponer de diferentes logros competitivos, como el de haber ganado 5 veces la medalla de oro en el Campeonato Mundial de Karate, o el haber ganado 13 veces la medalla de oro en el Campeonato Europeo de Karate, o haber sido capaz de ganar 4 veces el oro en la competición Karate1 Premier League. Actualmente es Cinturón Negro 5° Dan y compite como miembro del Qabala Club y de la Selección de Karate de Azerbaiyán, de la cual es capitán.</p>
+            </section>
         </main>
     )
 }
