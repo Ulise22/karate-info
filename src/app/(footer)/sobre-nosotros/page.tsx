46a76f813075292c82e472c5afa792ce4bb55e7b
Ulises Montenegro <ulimontenegro23@gmail.com>
2023-12-24 14:48:55 -0300

    sobre nosotros page done

--- src/app/(footer)/sobre-nosotros/page.tsx
+++ src/app/(footer)/sobre-nosotros/page.tsx
@@ -1,9 +1,10 @@
-
+import styles from './about.module.css'
 
 export default function SobreNosotros () {
     return(
-        <main>
-            
+        <main className={styles.about}>
+            <h1>Sobre Nosotros</h1>
+            <p>Desde este espacio estamos interesados en el karate-do y todo lo relacionado a este mundo, por eso buscamos aportar nuestro granito de arena a través de este medio para brindar toda la información disponible sobre los estilos de karate, la rica historia que este posee, los karatecas más destacados que hay, cuales son los katas y golpes más destacados y cómo ejecutarlos, e incluso que famosos actores o deportistas se beneficiaron de todo lo que el karate tiene para dar a quien lo practica. </p>
         </main>
     )
 }
