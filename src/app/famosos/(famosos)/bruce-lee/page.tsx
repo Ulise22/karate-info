5ca516c2af675212c4748c74673df9d8b18a6263
Ulises Montenegro <ulimontenegro23@gmail.com>
2023-12-26 11:24:38 -0300

    bruce lee article done

--- src/app/famosos/(famosos)/bruce-lee/page.tsx
+++ src/app/famosos/(famosos)/bruce-lee/page.tsx
@@ -7,7 +7,66 @@ export default function BruceLee () {
         <main className={styles.famoso}>
             <ArticlesHero title='Bruce Lee' img={lee} />
             <section className={styles.famoso__article}>
-                
+                <p>Bruce Lee es una de las mayores figuras y representantes de las artes marciales en el cine de historia, de hecho, debe ser el primero en que muchos piensan a la hora de buscar un artista marcial tan famoso en el cine no sólo Oriental sino que especialmente tuvo un impacto significativo en el mundo Occidental, gracias a sus películas y programas de televisión en los que participó, pero principalmente ganó notorierad devido a su carisma que a muchos contagio la pasión por las artes marciales alrededor de todo Occidente. A lo largo de su vida supo ser Mestro de artes marciales, actor, filósofo, guionista, director, escritor y productor, ganando reconocimientos por todo el mundo, incluída su propia <b>Estrella del Paseo de la Fama de Hollywood</b>.</p>
+                <p>Su nombre de nacimiento era Lee Jun-fan. El nombre con el que se lo conoce hoy en día, es la conformación de los 2 nombres &quot;Bruce&quot; y &quot;Lee&quot;, este último le fue puesto cuando se entregaron los papeles al Departamento de Trabajo de los Estados Unidos, dado que le pusieron un nombre chino y uno estadounidense, siendo el nombre chino Jun-Fan el que le fue dado por su madre y se le inscribió como Lee Jun-Fan, mientras que el nombre &quot;Bruce&quot; fue una sugerencia de la enfermera del hospital, con el objetivo de que el recién nacido tuviera un nombre occidental y así evitar problemas con su partida de nacimiento estadounidense. </p>
+                <p>Lee nació un 27 de noviembre de 1940 en el barrio estadounidense de Chinatown, San Francisco, California, y fue el cuarto de 5 hijos que tuvieron sus padres, Lee Hoi-chuen y Grace Ho. El nacimiento de Bruce en los Estados Unidos se dio de casualidad, ya que su padre, quien trabajaba como actor de películas cantonesas y comediante en la ópera china, se encontraba de gira presentando una obra de la &quot;Opera Company&quot; que pasó en aquel entonces por San Francisco.</p>
+                <p>Es por esto que pese a nacer allí, Bruce pasó la mayor parte de su infancia en Hong Kong. Los primeros años en que vivieron allí fueron muy duros porque coincidieron con la ocupación japonesa que vivió el país durante la Segunda Guerra Mundial (1939-1945), su madre pasaba mucho tiempo preocupada dado que al lado de la calle había un campamento militar japonés y constantemente eran retados a pelear por Bruce Lee, quien levantaba los puños en señal de pelea cuando los aviones de caza japoneses volaban a baja altitud.</p>
+                <p>A los 18 años Bruce vuelve a los Estados Unidos y aunque había participado ya en alrededor de 20 películas en Hong Kong, su llegada al mundo de Hollywood se dió cuando tomó un papel secundario en la serie televisiva <b>The Green Hornet</b>.</p>
+                <p>Su primera aparción en una película americana fue en 1969 cuando tomo un papel pequeño para la película <b>Marlowe</b>. Finalmente su exítoso en el mundo del cine llegaría cuando Bruce acepta la propuesta del productor Raymond Chow, de filmar la película <b>The Big Boss</b>. Dicha película había contado con un presupuesto de $100.000, y tan sólo en el primer día de estreno ya había recaudado $372.000 y terminaría recaudando un total de $3.200.000, siendo un éxito rotundo en taquilla y ganandole a Bruce una popularidad tremenda que lo llevaría a ser considerado un héroe nacional.</p>
+                <h2 className={styles.famoso__article__subtitle}>Bruce Lee Artes Marciales</h2>
+                <p>Durante su niñez, luego de que Bruce saliera ileso de una emboscada que le hicieron unos maleantes en que buscaban darle una paliza, su padre le enseñó las bases del arte marcial <b>Taichí Chuan</b> para que supiera defenderse. Luego de eso, gracias a que conocía a un chico de su edad que sabía Wing Chu, estuvo entre 3 y 4 años aprendiendo este estilo bajo la tutela de Ip Man, aunque pasaba la mayor parte de su entrenamiento que este chico que lo había iniciado en ese estilo de pelea.</p>
+                <p>Durante los 4 años que Bruce estudió en la universidad, tuvo que tomar varios trabajos que terminaría abandonando para poder ganarse la vida dando clases de <b>Wing Chun Kung-Fu</b> en diferentes lugares, incuídos una habitación sin ventanas que tomaba prestada, en parques públicos y en garajes vacíos los sábados.</p>
+                <p>Como no tenía amigos, comenzó a asistir a las reuniones de una sociedad china, donde había gente que también practicaba kung fu y otros estilos del norte de China, en los que predominaba el uso de las piernas, uso que Bruce desconocía, puesto que el wing chun trabaja piernas a un nivel muy bajo. Allí conoció a uno de sus mejores amigos, Jesse Glover, a quien Lee le enseñaría. </p>
+                <h2 className={styles.famoso__article__subtitle}>Bruce Lee Karate</h2>
+                <p>En 1964 Bruce asistió al campeonato <b>Long Beach Karate Tournament</b> organizado por el fundador del Kenpo Karate, Ed Parker. En dicho evento Lee sorprendió al público con sus habilidades y demostraciones, como hacer flexiones con 2 dedos de una mano y demostrando como hacer el <b>golpe de una pulgada</b>.</p>
+                <p>Fue en aquel campeonato de 1964 donde Lee se reunió por primera vez con Dan Inosanto, quien sirvió como sparring ocasional en las demostraciones que hacía Bruce. Fue quien además, motivado por la admiración que generaban las habilidades de Bruce Lee, se convertiría en alumno del mismo. &apos;No pude dormir pensando como ese hombre me derrotaba tan fácilmente, tenía la sensación de haber aprendido un oficio durante años, y que de repente, llega alguien y te dice: &quot;No le necesitamos más, queda despedido&quot;.&apos; Fueron las palabras de Inosanto sobre Bruce Lee.</p>
+                <h2 className={styles.famoso__article__subtitle}>Bruce Lee Muerte</h2>
+                <p>Justo cuando estaba en el mejor momento de su carrera, Bruce Lee había conseguido la oportunidad de destacarse en Hollywood y se estaba por estrenar la película coproducida por Warner Brothers <b>Operación Dragón</b> en agosto de 1973, ocurrió lo peor, Bruce Lee fallece el 20 de julio de 1973 con tan sólo 32 años de edad. </p>
+                <p>Aunque son muchas las teorías y las dudas que pueden surgir respecto a este tema, la versión oficial cuenta que su muerte se dió producto de un edema cerebral, causado a una hipersensibilidad alergica que tenía Bruce por el meprobamato, uno de los componentes químicos del Equagesic, un analgésico para el dolor de cabeza. </p>
+                <h2 className={styles.famoso__article__subtitle}>Bruce Lee Películas</h2>
+                <p>La filomagrafía de Bruce Lee consta de las siguientes producciones: </p>
+                <h3>Cine</h3>
+                <ul>
+                    <li>(1941)	Golden Gate Girl</li>
+                    <li>(1946)	The Birth of Mankind</li>
+                    <li>(1948)	Wealth is Like a Dream</li>
+                    <li>(1949)	Sai See in the Dream</li>
+                    <li>(1950)	The Kid</li>
+                    <li>(1950)  Infancy</li>
+                    <li>(1953)	A Myriad Homes</li>
+                    <li>(1953)  Blame it on Father</li>
+                    <li>(1953)  The Guiding Light</li>
+                    <li>(1953)  A Mother&apos;s Tears</li>
+                    <li>(1953)  In the Face of Demolition</li>
+                    <li>(1955)	An Orphan&apos;s Tragedy</li>
+                    <li>(1955)  Orphan&apos;s Song</li>
+                    <li>(1955)  Love</li>
+                    <li>(1955)  Love Part 2</li>
+                    <li>(1955)  We Owe It to Our Children</li>
+                    <li>(1956)	The Wise Guys Who Fool Around</li>
+                    <li>(1956)  Too Late For Divorce</li>
+                    <li>(1957)	The Thunderstorm</li>
+                    <li>(1957)  Darling Girl</li>
+                    <li>(1960)	The Orphan</li>
+                    <li>(1969)	Marlowe</li>
+                    <li>(1971)	The Big Boss</li>
+                    <li>(1972)	Fist of Fury</li>
+                    <li>(1972)  Way of the Dragon</li>
+                    <li>(1973)	Enter the Dragon</li>
+                    <li>(1978)	Game of Death</li>
+                </ul>
+                <h3>Televisión</h3>
+                <ul>
+                    <li>(1966-1967)	The Green Hornet</li>
+                    <li>(1966-1967)	Batman</li>
+                    <li>(1967)	Ironside</li>
+                    <li>(1969)	Blondie (en)</li>
+                    <li>(1969)	Here Come the Brides (en)</li>
+                    <li>(1971)	Longstreet (en)</li>
+                    <li>(1971)	The Pierre Berton Show (en)</li>
+                    <li>(1970-1973)	Enjoy Yourself Tonight (en)</li>
+                    <li>(2002)	The Art of Action: Martial Arts in Motion Picture</li>
+                </ul>
             </section>
         </main>
     )
--- src/app/famosos/(famosos)/dolph-lundgren/page.tsx
+++ src/app/famosos/(famosos)/dolph-lundgren/page.tsx
@@ -7,7 +7,100 @@ export default function Dolph () {
         <main className={styles.famoso}>
             <ArticlesHero title='Dolph Lundgren' img={dolph} />
             <section className={styles.famoso__article}>
-                
+                <p></p>
+                <h2 className={styles.famosos__article__subtitle}>Dolph Lundgren Películas</h2>
+                <p>Las películas en las que Dolph Lundgren ha participado, ya sea de forma protagonica o interpretando personajes secundarios son:</p>
+                <ul>
+                    <li>(1985) - A View to a Kill</li>
+                    <li>(1985) - Rocky IV</li>
+                    <li>(1987) - Masters of the Universe</li>
+                    <li>(1987) - Maximum Potential</li>
+                    <li>(1988) - Red Scorpion</li>
+                    <li>(1988) - R.P.G. II</li>
+                    <li>(1989) - The Punisher</li>
+                    <li>(1990) - I Come in Peace</li>
+                    <li>(1991) - I Come in Peace</li>
+                    <li>(1991) - Showdown in Little Tokyo</li>
+                    <li>(1992) - Universal Soldier</li>
+                    <li>(1993) - Joshua Tree</li>
+                    <li>(1994) - Sunny Side Up</li>
+                    <li>(1994) - Pentathlon</li>
+                    <li>(1994) - Men of War</li>
+                    <li>(1995) - Johnny Mnemonic</li>
+                    <li>(1995) - The Shooter</li>
+                    <li>(1996) - Silent Trigger</li>
+                    <li>(1997) - The Peacekeeper</li>
+                    <li>(1998) - The Minion</li>
+                    <li>(1998) - Sweepers</li>
+                    <li>(1999) - Bridge of Dragons</li>
+                    <li>(1999) - Storm Catcher</li>
+                    <li>(2000) - Jill Rips</li>
+                    <li>(2000) - The Last Warrior</li>
+                    <li>(2000) - Agent Red</li>
+                    <li>(2001) - Hidden Agenda</li>
+                    <li>(2003) - Detention</li>
+                    <li>(2004) - Direct Action</li>
+                    <li>(2004) - Fat Slags</li>
+                    <li>(2004) - Retrograde</li>
+                    <li>(2004) - The Defender</li>
+                    <li>(2005) - The Mechanik</li>
+                    <li>(2006) - The Inquiry</li>
+                    <li>(2007) - Diamond Dogs</li>
+                    <li>(2007) - Missionary Man</li>
+                    <li>(2009) - Direct Contact</li>
+                    <li>(2009) - Command Performance</li>
+                    <li>(2009) - Universal Soldier: Regeneration</li>
+                    <li>(2010) - Icarus</li>
+                    <li>(2010) - The Expendables</li>
+                    <li>(2011) - En el nombre del rey 2</li>
+                    <li>(2012) - Small Apartments</li>
+                    <li>(1012) - Stash House</li>
+                    <li>(2012) - One in the Chamber</li>
+                    <li>(2012) - The Expendables 2</li>
+                    <li>(2012) - Universal Soldier: Day of Reckoning</li>
+                    <li>(2013) - The Package</li>
+                    <li>(2013) - Legendary</li>
+                    <li>(2013) - Battle of the Damned</li>
+                    <li>(2013) - Ambushed</li>
+                    <li>(2013) - Blood of Redemption</li>
+                    <li>(2014) - Puncture Wounds</li>
+                    <li>(2014) - The Expendables 3</li>
+                    <li>(2014) - Skin Trade: Tráfico humano</li>
+                    <li>(2014) - Electric Boogaloo: The Wild, Untold Story of Cannon Films</li>
+                    <li>(2015) - War Pigs</li>
+                    <li>(2015) - The Good, the Bad and the Dead</li>
+                    <li>(2015) - Shark Lake</li>
+                    <li>(2015) - Riot</li>
+                    <li>(2015) - Malchishnik</li>
+                    <li>(2016) - Hail, Caesar!</li>
+                    <li>(2016) - Kindergarten Cop 2</li>
+                    <li>(2016) - Don&apos;t Kill It</li>
+                    <li>(2016) - Female Fight Club</li>
+                    <li>(2016) - Welcome To Willits</li>
+                    <li>(2017) - Larceny</li>
+                    <li>(2017) - Altitude</li>
+                    <li>(2017) - Dead Trigger</li>
+                    <li>(2017) - Stopping Traffic</li>
+                    <li>(2018) - Black Water</li>
+                    <li>(2018) - Creed II</li>
+                    <li>(2018) - Aquaman</li>
+                    <li>(2019) - The Tracker</li>
+                    <li>(2019) - Acceleration</li>
+                    <li>(2019) - Hard Night Falling</li>
+                    <li>(2019) - One Night: Joshua vs. Ruiz</li>
+                    <li>(2021) - Pups Alone</li>
+                    <li>(2021) - Seal Team</li>
+                    <li>(2021) - Castle Falls</li>
+                    <li>(2021) - The Making of &apos;Rocky vs. Drago&apos;</li>
+                    <li>(2022) - minions: The Rise of Gru</li>
+                    <li>(2022) - Section Eight</li>
+                    <li>(2022) - Operation Seawolf</li>
+                    <li>(2022) - Come Out Fighting</li>
+                    <li>(2023) - The Best Man</li>
+                    <li>(2023) - Expend4bles</li>
+                    <li>(2023) - Showdown at the Odessa</li>
+                    <li>(2023) - Aquaman y el Reino Perdido</li>
+                </ul>
             </section>
         </main>
     )
