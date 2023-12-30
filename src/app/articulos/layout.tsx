ac2b86f7604162717da40bcf6f53cbaebe1c5490
Ulises Montenegro <ulimontenegro23@gmail.com>
2023-12-30 10:55:20 -0300

    layaout created

---
+++ src/app/articulos/layout.tsx
@@ -0,0 +1,13 @@
+
+
+export default function ArticulosLayout ({
+    children,
+  }: {
+    children: React.ReactNode
+  })  {
+    return(
+        <>
+            {children}
+        </>
+    )
+}
