92cb7adc8b7b0d0d5e7f7ab288c5bc436a9d42e7
Ulises Montenegro <ulimontenegro23@gmail.com>
2024-01-04 13:42:41 -0300

    added script from google

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,6 +21,7 @@ export default function RootLayout({
       <head>
         <link rel='icon' href='/KI.svg' />
         <meta name="google-adsense-account" content="ca-pub-9270621638584149"></meta>
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9270621638584149" crossOrigin="anonymous"></script>
       </head>
       <body className={inter.className}>
         <Navbar />
