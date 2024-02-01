440d613ed291be01a12d7cc4f23a5a643a7198a7
Ulises Montenegro <ulimontenegro23@gmail.com>
2024-02-01 13:32:24 -0300

    taylor-lautner article done

--- src/app/famosos/(famosos)/taylor-lautner/page.tsx
+++ src/app/famosos/(famosos)/taylor-lautner/page.tsx
@@ -8,7 +8,51 @@ export default function TaylorLauter () {
         <main className={styles.famoso}>
             <ArticlesHero title='Taylor Lautner' img={taylor} />
             <section className={styles.famoso__article}>
-                <p></p>
+                <p>Taylor Lautner es un conocido actor, modelo y luchador de artes marciales estadounidense, que adquirió fama mundial especialmente con su papel como <b>Jacob Black</b>, en la serie de películas <b>Twilight</b> o <b>Crepúsculo</b> en español, rol que comenzó a interpretar en 2008.</p>
+                <p>Taylor Daniel Lautner, nació el 11 de febrero de 1992 en la ciudad de Michigan, Grand Rapids, en los Estados Unidos, ciudad donde creció y pasó casi toda su infancia.</p>
+                <p>En el año 2001 se mudó a Los Ángeles, donde comenzó su carrera como actor interpretando pequeños papeles en películas y anuncios comerciales, teniendo su primera aparición en una película para televisión llamada <b>Shadow Fury</b>. Luego conseguiría un trabajo como actor de doblaje en un comercial, que le abriría las puertas para luego hacer doblajes en series animadas como <b>Danny Phantom</b>, <b>Duck Dodgers</b> y <b>What&apos;s New, Scooby-Doo?</b>. Hasta que finalmente conseguiría su primer papel destacado en la película <b>Las aventuras de Sharkboy y Lavagirl</b>, donde coreografió todas sus escenas de lucha, luego de que el director se enterara de su extenso entrenamiento en artes marciales, especialmente en el karate.</p>
+                <p>Hasta que finalmente, en el año 2008, participaría en la película que le daría fama mundial, al audicionar y quedarse con el papel de <b>Jacob Black</b> en la película <b>Twilight</b>, que fue un éxito comercial, ganando &69 millones en su primer fin de semana, y $392 millones en todo el mundo.</p>
+                <p>En cuanto a su vida personal, luego de las varias relaciones que mantuvo con celebridades reconocidas como con la cantante <b>Taylor Swift</b>, actualmente y desde 2018 se encuentra en una relación con Taylor Dome, que es una enfermera de California con quien se casó en noviembre de 2022.</p>
+                <h2>Taylor Lautner Karate</h2>
+                <p>Taylor Lautner estudió karate desde los 6 a los 13 años en la escuela Fabiano&apos;s Karate School en Holland, Michigan, y con tan sólo un año de entrenamiento ya estaba ganando campeonatos. Debido a esto fue invitado a entrenar con el 7 veces campeón del mundo de karate <b>Michael Chaturantabut</b>. </p>
+                <p>A los 8 años se le invitó a representar a su país en la World Karate Association donde, en la modalidad Junior World Forms and Weapons, donde ganó tres medallas de oro. Seguiría destacando después de esto, dado que con tan sólo 11 años se posicionó como número uno en el mundo del NASKA&apos;s Black Belt Open Forms, Musical Weapons, Traditional Weapons y Traditional Forms. Finalmente, a la edad de 12 años, ganaría el Campeonato del Mundo Junior de Artes Marciales.</p>
+                <p>Según lo que le comentó a <b>Men&apos;s Health</b>, a día de hoy sigue practicando artes marciales con regularidad. Además de que sostiene una dieta específica y se mantiene alejado del alcohol y las drogas para mantener su cuerpo sano y en forma, como acostumbra a hacer en sus películas.</p>
+                <h2>Taylor Lautner Filmografía</h2>
+                <p>Taylor tuvo una reconocida carrera en el mundo audiovisual, destacando especialmente el éxito comercial que fue su participación en la saga de películas de <b>Twilight</b>. La lista de películas y series y programas de televisión en los que participó, consta de: </p>
+                <h3>Cine</h3>
+                <ul>
+                    <li>Shadow Fury</li>
+                    <li>Las aventuras de Sharkboy y Lavagirl</li>
+                    <li>Cheaper by the Dozen 2</li>
+                    <li>Crepúsculo</li>
+                    <li>The Twilight Saga: New Moon</li>
+                    <li>Valentine&apos;s Day</li>
+                    <li>The Twilight Saga: Eclipse</li>
+                    <li>Field of Dreams 2: Lockout</li>
+                    <li>Abduction</li>
+                    <li>The Twilight Saga: Breaking Dawn - Part 1</li>
+                    <li>The Twilight Saga: Breaking Dawn - Part 2</li>
+                    <li>Grown Ups 2</li>
+                    <li>Tracers</li>
+                    <li>The Ridiculous 6</li>
+                    <li>Run the Tide</li>
+                    <li>Home Team</li>
+                </ul>
+                <h3>Televisión</h3>
+                <ul>
+                    <li>The Bernie Mac Show</li>
+                    <li>The Nick & Jessica Variety Hour</li>
+                    <li>My Wife and Kids</li>
+                    <li>Summerland</li>
+                    <li>¿Qué hay de nuevo, Scooby-Doo?</li>
+                    <li>Duck Dodgers</li>
+                    <li>Danny Phantom</li>
+                    <li>Love, Inc.</li>
+                    <li>He&apos;s a Bully, Charlie Brown</li>
+                    <li>My Own Worst Enemy</li>
+                    <li>Cuckoo</li>
+                    <li>Scream Queens</li>
+                </ul>
             </section>
         </main>
     )
